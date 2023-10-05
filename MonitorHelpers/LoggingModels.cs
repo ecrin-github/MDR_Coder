743696074a176a5416d984568888c9f16f922ac7
scanhamman <stevecanham@outlook.com>
2023-10-05 18:43:43 +0200

    Minor tidying

--- MonitorHelpers/LoggingModels.cs
+++ MonitorHelpers/LoggingModels.cs
@@ -2,43 +2,6 @@
 
 namespace MDR_Coder;
 
-[Table("sf.source_parameters")]
-public class SourceX
-{
-    public int id { get; set; }
-    public string? source_type { get; }
-    public int? preference_rating { get; set; }
-    public string? database_name { get; set; }
-    public string? db_conn { get; set; }
-    public int? default_harvest_type_id { get; set; }
-    public bool? requires_file_name { get; set; }
-    public bool? uses_who_harvest { get; set; }
-    public int? harvest_chunk { get; }
-    public string? local_folder { get; set; }
-    public bool? local_files_grouped { get; set; }
-    public int? grouping_range_by_id { get; set; }
-    public string? local_file_prefix { get; set; }
-    public bool? has_study_tables { get; set; }
-    public bool? has_study_topics { get; set; }
-    public bool? has_study_conditions { get; }
-    public bool? has_study_features { get; set; }
-    public bool? has_study_iec{ get; }
-    public string? study_iec_storage_type { get; }
-    public bool? has_study_organisations { get; }
-    public bool? has_study_people { get; }
-    public bool? has_study_references { get; set; }
-    public bool? has_study_relationships { get; set; }
-    public bool? has_study_links { get; set; }
-    public bool? has_study_countries { get; set; }
-    public bool? has_study_locations { get; set; }
-    public bool? has_study_ipd_available { get; set; }
-    public bool? has_object_datasets { get; set; }
-    public bool? has_object_dates { get; set; }
-    public bool? has_object_relationships { get; set; }
-    public bool? has_object_rights { get; set; }
-    public bool? has_object_pubmed_set { get; set; }
-    public bool? has_object_bbmri_set { get; }
-}
 
 [Table("sf.source_parameters")]
 public class Source
